d02fefafc6d484d2158e9aa69ae7fd22416d1ec7
elbadritarek <elbadritarek612@gmail.com>
2023-12-04 01:46:12 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main(List<String> args) {
+  runApp(chatApp());
+}
+
+class chatApp extends StatelessWidget {
+  const chatApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(),
+    );
+  }
+}
